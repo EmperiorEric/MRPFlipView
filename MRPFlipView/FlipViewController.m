b662187bf2b9d778d7dc7babfe5a4307afa35c1b
Ryan Poolos <ryanpoolos@Gmail.com>
2013-06-25 17:46:49 -0400

    Adjusted positions and colors in demo viewController.

--- MRPFlipView/FlipViewController.m
+++ MRPFlipView/FlipViewController.m
@@ -28,22 +28,23 @@
     
     [self.view setBackgroundColor:[UIColor colorWithWhite:0.99 alpha:1.0]];
     
-    CGFloat width = CGRectGetWidth(self.view.bounds) / 2.0;
-    CGFloat height = CGRectGetHeight(self.view.bounds) / 2.0;
+    CGFloat padding = 48.0;
+    CGFloat width = ((CGRectGetWidth(self.view.bounds) - (padding * 3.0)) / 2.0);
+    CGFloat height = ((CGRectGetHeight(self.view.bounds) - (padding * 3.0)) / 2.0);
     
-    MRPFlipView *flipViewA = [[MRPFlipView alloc] initWithFrame:CGRectMake(0.0, 0.0, width, height)];
+    MRPFlipView *flipViewA = [[MRPFlipView alloc] initWithFrame:CGRectMake(padding, padding, width, height)];
     [flipViewA setAutoresizingMask:UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth];
     [self.view addSubview:flipViewA];
     
-    MRPFlipView *flipViewB = [[MRPFlipView alloc] initWithFrame:CGRectMake(width, 0.0, width, height)];
+    MRPFlipView *flipViewB = [[MRPFlipView alloc] initWithFrame:CGRectMake(CGRectGetMaxX(flipViewA.frame) + padding, padding, width, height)];
     [flipViewB setAutoresizingMask:UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleBottomMargin|UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth];
     [self.view addSubview:flipViewB];
     
-    MRPFlipView *flipViewC = [[MRPFlipView alloc] initWithFrame:CGRectMake(0.0, height, width, height)];
+    MRPFlipView *flipViewC = [[MRPFlipView alloc] initWithFrame:CGRectMake(padding, CGRectGetMaxY(flipViewA.frame) + padding, width, height)];
     [flipViewC setAutoresizingMask:UIViewAutoresizingFlexibleRightMargin|UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth];
     [self.view addSubview:flipViewC];
     
-    MRPFlipView *flipViewD = [[MRPFlipView alloc] initWithFrame:CGRectMake(width, height, width, height)];
+    MRPFlipView *flipViewD = [[MRPFlipView alloc] initWithFrame:CGRectMake(CGRectGetMaxX(flipViewC.frame) + padding, CGRectGetMaxY(flipViewB.frame) + padding, width, height)];
     [flipViewD setAutoresizingMask:UIViewAutoresizingFlexibleLeftMargin|UIViewAutoresizingFlexibleTopMargin|UIViewAutoresizingFlexibleHeight|UIViewAutoresizingFlexibleWidth];
     [self.view addSubview:flipViewD];
     
@@ -51,14 +52,12 @@
     for (MRPFlipView *flipView in @[flipViewA, flipViewB, flipViewC, flipViewD]) {
         flipView.delegate = self;
         
-        [flipView setBackgroundColor:[UIColor colorWithWhite:0.9 alpha:1.0]];
+        [flipView setBackgroundColor:[UIColor colorWithWhite:0.0 alpha:0.1]];
         
-        [flipView.frontView setBackgroundColor:[[UIColor redColor] colorWithAlphaComponent:0.35]];
-        [flipView.backView setBackgroundColor:[[UIColor blueColor] colorWithAlphaComponent:0.35]];
-        
-        [flipView.frontView setFrame:CGRectInset(flipView.frontView.superview.bounds, 4.0, 4.0)];
-        [flipView.backView setFrame:CGRectInset(flipView.backView.superview.bounds, 4.0, 4.0)];
-        
+        [flipView.frontView setBackgroundColor:[[UIColor redColor] colorWithAlphaComponent:0.5]];
+        [flipView.backView setBackgroundColor:[[UIColor blueColor] colorWithAlphaComponent:0.5]];
+                
+        flipView.layer.cornerRadius = 4.0;
         flipView.frontView.layer.cornerRadius = 4.0;
         flipView.backView.layer.cornerRadius = 4.0;
     }
