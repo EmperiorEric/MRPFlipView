605c4937d86ac919d1ac4928b91e173cfafbc942
Ryan Poolos <ryanpoolos@Gmail.com>
2013-06-25 17:20:27 -0400

    Initial Commit

---
+++ MRPFlipView/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MRPFlipView
+//
+//  Created by Ryan Poolos on 6/25/13.
+//  Copyright (c) 2013 Frozen Fire Studios. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
